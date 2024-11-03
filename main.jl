fc6de7be4061c896cf7b9fc5cf611c65e0c39033
novux <julianmcclelland3@gmail.com>
2024-11-03 01:54:04 -0700

    fix spelling error and add data size check

--- main.jl
+++ main.jl
@@ -1,4 +1,9 @@
-using LinearAlgebra: norm2
+using Pkg
+Pkg.activate(".")
+Pkg.add("LinearAlgebra")
+using LinearAlgebra
+Pkg.add("CUDA")
+using CUDA
 
 function forward(layers, weights)
   for i in 1:(length(layers)-1)
@@ -11,7 +16,7 @@ function loss(layers, weights, data)
   for i in 1:(length(data))
     layers[1] = data[i][1]
     forward(layers, weights)
-    averageLoss += (0.5 / length(data)) * norm2(data[i][2] - layers[length(layers)])^2
+    averageLoss += (0.5 / length(data)) * LinearAlgebra.norm(data[i][2] - layers[length(layers)])^2
   end
   return averageLoss
 end
@@ -19,31 +24,38 @@ end
 sigmoid(x) = 1 / (1 + exp(-x))
 
 function (@main)(args)
-  architecture = [1, 2, 2, 1]
+  architecture = [2, 2, 2, 1]
+  # weights = Vector{CuMatrix{Float64}}(undef, length(architecture) - 1)
+  # weightsPrime = Vector{CuMatrix{Float64}}(undef, length(architecture) - 1)
   weights = Vector{Matrix{Float64}}(undef, length(architecture) - 1)
   weightsPrime = Vector{Matrix{Float64}}(undef, length(architecture) - 1)
   for index in 1:(length(architecture)-1)
+    # weights[index] = CUDA.rand(architecture[index+1], architecture[index])
+    # weightsPrime[index] = CUDA.zeros(architecture[index+1], architecture[index])
     weights[index] = rand(architecture[index+1], architecture[index])
     weightsPrime[index] = zeros(architecture[index+1], architecture[index])
   end
+  # layers = Vector{CuVector{Float64}}(undef, length(architecture))
   layers = Vector{Vector{Float64}}(undef, length(architecture))
   for (index, value) in enumerate(architecture)
+    # layers[index] = CUDA.zeros(value)
     layers[index] = zeros(value)
   end
 
   data = [
-    [[0.0], [0.0]],
-    [[0.1], [0.2]],
-    [[0.2], [0.4]],
-    [[0.3], [0.6]],
-    [[0.4], [0.8]],
-    [[0.5], [1.0]],
+    [[0.0, 0.0], [0.0]],
+    [[1.0, 0.0], [1.0]],
+    [[0.0, 1.0], [1.0]],
+    [[1.0, 1.0], [1.0]],
   ]
+
+  @assert length(data[1][1]) == architecture[1] "Number of data input(s) does not match architecture"
+  @assert length(data[1][2]) == architecture[length(architecture)] "Number of data output(s) does not match architecture"
 
   epochs = 100000
   deltaX = 0.0001
   learningRate = 1
-  for epoch in 1:epochs
+  @time for epoch in 1:epochs
     # calculate weights prime
     for (weightLayer, weight) in enumerate(weights)
       for row in 1:size(weight)[1]
@@ -62,9 +74,9 @@ function (@main)(args)
   end
 
   println("--Test Training Results--")
-  println("Final Traingin Error: ", loss(layers, weights, data))
+  println("Final Trainging Error: ", loss(layers, weights, data))
 
-  layers[1] = [0.3]
+  layers[1] = [0.0, 1.0]
   forward(layers, weights)
   println("--Test Input--")
   println("Input: ", layers[1])
