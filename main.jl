6a8b650d3cb3d8d4b6a54c50122c09398c64f8f6
novux <julianmcclelland3@gmail.com>
2024-11-05 00:24:40 -0800

    removed finite differences, added real derivative

--- main.jl
+++ main.jl
@@ -1,9 +1,6 @@
 using Pkg
 Pkg.activate(".")
-Pkg.add("LinearAlgebra")
 using LinearAlgebra
-Pkg.add("CUDA")
-using CUDA
 
 function forward(layers, weights)
   for i in 1:(length(layers)-1)
@@ -22,23 +19,20 @@ function loss(layers, weights, data)
 end
 
 sigmoid(x) = 1 / (1 + exp(-x))
+sigmoidPrime(x) = sigmoid(x) * (1 - sigmoid(x))
+sigmoidV(v) = map(x -> sigmoid(x), v)
+sigmoidPrimeV(v) = map(x -> sigmoidPrime(x), v)
 
 function (@main)(args)
-  architecture = [2, 2, 2, 1]
-  # weights = Vector{CuMatrix{Float64}}(undef, length(architecture) - 1)
-  # weightsPrime = Vector{CuMatrix{Float64}}(undef, length(architecture) - 1)
+  architecture = [2, 2, 1]
   weights = Vector{Matrix{Float64}}(undef, length(architecture) - 1)
   weightsPrime = Vector{Matrix{Float64}}(undef, length(architecture) - 1)
   for index in 1:(length(architecture)-1)
-    # weights[index] = CUDA.rand(architecture[index+1], architecture[index])
-    # weightsPrime[index] = CUDA.zeros(architecture[index+1], architecture[index])
     weights[index] = rand(architecture[index+1], architecture[index])
     weightsPrime[index] = zeros(architecture[index+1], architecture[index])
   end
-  # layers = Vector{CuVector{Float64}}(undef, length(architecture))
   layers = Vector{Vector{Float64}}(undef, length(architecture))
   for (index, value) in enumerate(architecture)
-    # layers[index] = CUDA.zeros(value)
     layers[index] = zeros(value)
   end
 
@@ -53,19 +47,19 @@ function (@main)(args)
   @assert length(data[1][2]) == architecture[length(architecture)] "Number of data output(s) does not match architecture"
 
   epochs = 100000
-  deltaX = 0.0001
   learningRate = 1
   @time for epoch in 1:epochs
     # calculate weights prime
-    for (weightLayer, weight) in enumerate(weights)
-      for row in 1:size(weight)[1]
-        for column in 1:size(weight)[2]
-          weightsCopy = deepcopy(weights)
-          weightsCopy[weightLayer][row, column] += deltaX
-          weightsPrime[weightLayer][row, column] = (loss(layers, weightsCopy, data) - loss(layers, weights, data)) / deltaX
-        end
-      end
-    end
+    currentData = data[(epoch%length(data)+1)][2]
+    layers[1] = data[(epoch%length(data)+1)][1]
+    forward(layers, weights)
+    weightsPrime[2] = (layers[3] - currentData) * sigmoidV(layers[2])'
+    weightsPrime[1] = diagm(sigmoidPrimeV(layers[2])) * weights[2]' * (layers[3] - currentData) * sigmoidV(layers[1])'
+
+    # this is for 4 layers
+    # weightsPrime[3] = (layers[4] - currentData) * sigmoidV(layers[3])'
+    # weightsPrime[2] = diagm(sigmoidPrimeV(layers[3])) * weights[3]' * (layers[4] - currentData) * sigmoidV(layers[2])'
+    # weightsPrime[1] = diagm(sigmoidPrimeV(layers[2])) * weights[2]' * diagm(sigmoidPrimeV(layers[3])) * weights[3]' * (layers[4] - currentData) * sigmoidV(layers[1])'
 
     # desend
     for (weightLayer, weight) in enumerate(weights)
@@ -76,10 +70,11 @@ function (@main)(args)
   println("--Test Training Results--")
   println("Final Trainging Error: ", loss(layers, weights, data))
 
-  layers[1] = [0.0, 1.0]
-  forward(layers, weights)
   println("--Test Input--")
-  println("Input: ", layers[1])
-  println("Ouput: ", layers[length(layers)])
-
+  for input in data
+    layers[1] = input[1]
+    forward(layers, weights)
+    println("Input: ", layers[1])
+    println("Ouput: ", layers[length(layers)])
+  end
 end
